41fbaa3c496e46d020f5b381df9e730be2c20a04
guillemrull111 <guillem.rull@estudiant.enti.cat>
2025-12-18 10:18:44 +0100

    g

--- Ajedrezfinal/Ajedrezfinal/Gameplay.cpp
+++ Ajedrezfinal/Ajedrezfinal/Gameplay.cpp
@@ -8,30 +8,61 @@ void MovimientoFichas(char tablero[FILA][COLUMNA]) {
 	int filaOrigen, colOrigen;
 	int filaDestino, colDestino;
 	bool juegoAcabado = false;
+	bool turno = false;
 
 	while (!juegoAcabado) {
+		while (turno == false) {
 
-		std::cout << "Fila de la ficha a mover: ";
-		std::cin >> filaOrigen;
-		std::cout << "Columna de la ficha a mover: ";
-		std::cin >> colOrigen;
+			std::cout << "Fila de la ficha a mover: ";
+			std::cin >> filaOrigen;
+			std::cout << "Columna de la ficha a mover: ";
+			std::cin >> colOrigen;
 
-		if (tablero[filaOrigen][colOrigen] == 'p') {
+			if (tablero[filaOrigen][colOrigen] == 'p') {
 
-			std::cout << "Fila destino: ";
-			std::cin >> filaDestino;
-			std::cout << "Columna destino: ";
-			std::cin >> colDestino;
+				std::cout << "Fila destino: ";
+				std::cin >> filaDestino;
+				std::cout << "Columna destino: ";
+				std::cin >> colDestino;
 
-			// Mover ficha
-			tablero[filaOrigen][colOrigen] = '*';
-			tablero[filaDestino][colDestino] = 'p';
+				// Mover ficha
+				tablero[filaOrigen][colOrigen] = '*';
+				tablero[filaDestino][colDestino] = 'p';
 
+			}
+			else {
+				std::cout << "No hay ficha en esa posicion.\n";
+			}
+			system("cls");
+			ImprimirTablero(tablero);
 		}
-		else {
-			std::cout << "No hay ficha en esa posicion.\n";
+		turno = true;
+
+		while (turno == true)
+		{
+			std::cout << "Fila de la ficha a mover: ";
+			std::cin >> filaOrigen;
+			std::cout << "Columna de la ficha a mover: ";
+			std::cin >> colOrigen;
+
+			if (tablero[filaOrigen][colOrigen] == 'p') {
+
+				std::cout << "Fila destino: ";
+				std::cin >> filaDestino;
+				std::cout << "Columna destino: ";
+				std::cin >> colDestino;
+
+				// Mover ficha
+				tablero[filaOrigen][colOrigen] = '*';
+				tablero[filaDestino][colDestino] = 'p';
+
+			}
+			else {
+				std::cout << "No hay ficha en esa posicion.\n";
+			}
+			system("cls");
+			ImprimirTablero(tablero);
 		}
-		system("cls");
-		ImprimirTablero(tablero);
+		turno = false;
 	}
-}
+}	
