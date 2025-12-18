4e7f9807e84d88be0e732f60dfae0e7571f6b75a
guillemrull111 <guillem.rull@estudiant.enti.cat>
2025-12-18 09:46:15 +0100

    g

--- Ajedrezfinal/Ajedrezfinal/Gameplay.cpp
+++ Ajedrezfinal/Ajedrezfinal/Gameplay.cpp
@@ -5,19 +5,27 @@ void MovimientoFichas(char tablero[FILA][COLUMNA]) {
 
 	int FilafichaUsuario;
 	int ColumnafichaUsuario;
+	bool turno = false;
+	bool juegoAcabado = false;
 
-	
-	std::cout << "Escoje la fils de la ficha que quieres mover: ";
-	std::cin >> FilafichaUsuario;
+	while (juegoAcabado == false) {
+		while (turno == false)
+		{
+			std::cout << "Escoje la fils de la ficha que quieres mover: ";
+			std::cin >> FilafichaUsuario;
 
-	std::cout << "Escoje la columna de la ficha que quieres mover: ";
-	std::cin >> ColumnafichaUsuario;
+			std::cout << "Escoje la columna de la ficha que quieres mover: ";
+			std::cin >> ColumnafichaUsuario;
 
 
-	std::cout << "\nDonde lo quieres mover: ";
-	std::cin >> posicionMover;
-	if (tablero[FilafichaUsuario][ColumnafichaUsuario])
-	{
+			std::cout << "\nDonde lo quieres mover: ";
+			std::cin >> posicionMover;
+			if (tablero[FilafichaUsuario][ColumnafichaUsuario])
+			{
 
+			}
+		}
 	}
+	
+	
 }
